5b644a69acd449f37ef309455d9d8a718aab5b5e
seva <lopachuk.seva@gmail.com>
2023-06-30 23:31:51 +0200

    removed unused imports

--- src/config.py
+++ src/config.py
@@ -16,8 +16,5 @@ DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
 DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
 DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
 
-SECRET1 = os.environ.get("SECRET1")
-SECRET2 = os.environ.get("SECRET2")
-
 STATION_ACCESS_KEY = os.environ.get("STATION_ACCESS_KEY")
 USER_ACCESS_KEY = os.environ.get("USER_ACCESS_KEY")
